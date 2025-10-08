045c0926300a25c02c8bd79cea0b866ec75a7acc
AGuoCode <aguo.coding@gmail.com>
2025-10-09 00:05:25 +0700

    Update Design

--- src/main/java/com/guai/onlinelearningapi/OnlineLearningApiApplication.java
+++ src/main/java/com/guai/onlinelearningapi/OnlineLearningApiApplication.java
@@ -2,8 +2,10 @@ package com.guai.onlinelearningapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class OnlineLearningApiApplication {
 
 	public static void main(String[] args) {
